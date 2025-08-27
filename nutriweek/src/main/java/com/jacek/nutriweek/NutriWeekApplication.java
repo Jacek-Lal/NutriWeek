441aab65a7367254dcc4a2a4d36e5fd408e98721
Jacek <laljacek@gmail.com>
2025-08-27 11:19:01 +0200

    project setup

---
+++ nutriweek/src/main/java/com/jacek/nutriweek/NutriWeekApplication.java
@@ -0,0 +1,13 @@
+package com.jacek.nutriweek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriWeekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutriWeekApplication.class, args);
+	}
+
+}
