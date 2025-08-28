88b79a321866419dac8569be2b7e719108e6c1a7
Jacek <laljacek@gmail.com>
2025-08-28 22:50:41 +0200

    added tailwind styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { searchProducts } from "./api/ProductService";
 import Product from "./components/Product";
 
 function App() {
   const [data, setData] = useState({});
+  const [input, setInput] = useState("");
 
   const getProducts = async (query, page = 0, size = 10) => {
     try {
@@ -16,19 +17,22 @@ function App() {
     }
   };
 
-  // useEffect(() => {
-  //   getProducts("cheese");
-  // }, []);
-
   return (
     <>
-      <div role="search" className="search__wrapper">
-        <input className="search-bar"></input>
-        <button className="bg-green-500">
+      <div role="search" className="flex justify-center items-center gap-2 p-6">
+        <input
+          className="w-1/3 rounded-xl border border-gray-300 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 shadow-sm"
+          value={input}
+          onInput={(e) => setInput(e.target.value)}
+        ></input>
+        <button
+          className="flex items-center gap-2 rounded-xl bg-blue-600 px-4 py-2 text-white shadow-md hover:bg-blue-700 active:scale-95 transition"
+          onClick={() => getProducts(input)}
+        >
           <i className="magnifier"></i> Search
         </button>
       </div>
-      <ul>
+      <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6">
         {data?.foods?.length > 0 &&
           data.foods.map((product) => (
             <Product product={product} key={product.fdcId} />
--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -2,14 +2,26 @@ import React from "react";
 
 const Product = ({ product }) => {
   return (
-    <div>
-      <h1>{product.description}</h1>
-      {product.foodNutrients.slice(1, 5).map((n) => (
-        <p key={n.nutrientId}>
-          {n.nutrientName}: {n.value} {n.unitName}
-        </p>
-      ))}
-    </div>
+    <li className="rounded-2xl border border-gray-200 bg-white p-5 shadow hover:shadow-lg transition">
+      <div className="flex bg-red-200">
+        <h1 className="bg-blue-200 text-lg font-semibold text-gray-800 mb-3">
+          {product.description}
+        </h1>
+        <button>
+          <i class="bi bi-plus-circle"></i>
+        </button>
+      </div>
+      <div className="space-y-1 text-sm text-gray-600">
+        {product.foodNutrients.slice(1, 5).map((n) => (
+          <p key={n.nutrientId} className="flex justify-between">
+            <span>{n.nutrientName}</span>
+            <span className="font-medium">
+              {n.value} {n.unitName}
+            </span>
+          </p>
+        ))}
+      </div>
+    </li>
   );
 };
 
