299171141c40f64646259b43c6a9bb0996f4e2b3
Jacek <laljacek@gmail.com>
2025-11-06 23:59:25 +0100

    delete unenabled users

--- backend/src/main/java/com/jacek/nutriweek/auth/repository/TokenRepository.java
+++ backend/src/main/java/com/jacek/nutriweek/auth/repository/TokenRepository.java
@@ -7,10 +7,17 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.Instant;
+import java.util.List;
 import java.util.Optional;
 
 public interface TokenRepository extends JpaRepository<VerificationToken, Long> {
     Optional<VerificationToken> findByToken(String token);
+
+    @Query("""
+            select vt.user from VerificationToken vt
+            where vt.expirationDate < :now and vt.user.enabled = false
+            """)
+    List<Long> findExpired(@Param("now") Instant now);
 
     @Modifying
     @Query("delete from VerificationToken vt where vt.expirationDate < :now")
--- backend/src/main/java/com/jacek/nutriweek/auth/service/TokenCleanupService.java
+++ backend/src/main/java/com/jacek/nutriweek/auth/service/TokenCleanupService.java
@@ -1,21 +1,30 @@
 package com.jacek.nutriweek.auth.service;
 
 import com.jacek.nutriweek.auth.repository.TokenRepository;
+import com.jacek.nutriweek.user.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(rollbackOn = Exception.class)
 public class TokenCleanupService {
     private final TokenRepository tokenRepository;
+    private final UserRepository userRepository;
 
     @Scheduled(cron = "0 0 * * * *")
     public void deleteExpiredTokens(){
-        tokenRepository.deleteExpired(Instant.now());
+        Instant now = Instant.now();
+        List<Long> expiredUserIds = tokenRepository.findExpired(now);
+
+        if (!expiredUserIds.isEmpty()) {
+            userRepository.deleteByIdIn(expiredUserIds);
+            tokenRepository.deleteExpired(now);
+        }
     }
 }
