9efa76827aff89ad4b8260215996bb9062d04ecc
Jacek Lal <61835186+Jacek-Lal@users.noreply.github.com>
2025-11-12 01:10:34 +0100

    added necessary import

--- backend/src/main/java/com/jacek/nutriweek/common/config/SecurityConfig.java
+++ backend/src/main/java/com/jacek/nutriweek/common/config/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.security.web.context.SecurityContextRepository;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 
