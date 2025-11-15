c7a7b651b2f2a8a37188bb864433186a05337c00
Jacek <laljacek@gmail.com>
2025-11-15 16:32:27 +0100

    getting csrf token when registering

--- frontend/src/api/AuthService.js
+++ frontend/src/api/AuthService.js
@@ -6,8 +6,9 @@ export const getCsrf = async () => {
   const { data } = await api.get(`${ENDPOINT}/csrf`);
   setCsrfToken(data.token); 
 };
-export const registerUser = (payload) =>
-  api.post(`${ENDPOINT}/register`, payload);
+export const registerUser = (payload) => {
+  getCsrf().then(() => api.post(`${ENDPOINT}/register`, payload));
+}
 export const loginUser = (payload) => api.post(`${ENDPOINT}/login`, payload);
 export const logoutUser = () => api.post(`${ENDPOINT}/logout`, {});
 export const getUser = () => api.get(`${ENDPOINT}/me`);
