bbc116c8376f3fcf20d713a6b4e3aa17b8a883ff
Jacek <laljacek@gmail.com>
2025-11-29 19:45:19 +0100

    fixed register response bug

--- frontend/src/api/AuthService.js
+++ frontend/src/api/AuthService.js
@@ -4,12 +4,16 @@ const ENDPOINT = ENDPOINTS.AUTH;
 
 export const getCsrf = async () => {
   const { data } = await api.get(`${ENDPOINT}/csrf`);
-  setCsrfToken(data.token); 
+  setCsrfToken(data.token);
 };
-export const registerUser = (payload) => {
-  getCsrf().then(() => api.post(`${ENDPOINT}/register`, payload));
-}
+export const registerUser = async (payload) => {
+  await getCsrf();
+  return api.post(`${ENDPOINT}/register`, payload);
+};
 export const loginUser = (payload) => api.post(`${ENDPOINT}/login`, payload);
-export const loginDemoUser = () => getCsrf().then(() => api.post(`${ENDPOINT}/demo`));
+export const loginDemoUser = async () => {
+  await getCsrf();
+  return api.post(`${ENDPOINT}/demo`);
+};
 export const logoutUser = () => api.post(`${ENDPOINT}/logout`, {});
 export const getUser = () => api.get(`${ENDPOINT}/me`);
